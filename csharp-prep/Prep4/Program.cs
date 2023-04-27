347e209e412dbdee158350ef9c746683413f092c
David Basante <dsbasante@gmail.com>
2023-04-27 19:17:57 -0300

    Commit list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,34 @@
-using System;
+using System.Collections.Generic;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+List<int> num = new List<int>();
+
+int number = -1;
+
+while(number != 0){
+    Console.Write("Enter a number (Type 0 to quit): ");
+    number = int.Parse(Console.ReadLine());
+
+    if(number != 0){
+
+        num.Add(number);
     }
+
 }
+
+int all = 0;
+foreach (int numbers in num){
+    all++;
+}
+
+Console.WriteLine($"Total of numbers:{all}");
+
+
+int largest = num[0];
+foreach(int numbers in num){
+    if(numbers > largest){
+        largest = numbers;
+   }
+}
+Console.WriteLine($"The largest number is {largest}");
+
+Console.ReadKey();
