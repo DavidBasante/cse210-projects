14091a82a587971d2f8c7dc791936705ff3cba89
David Basante <dsbasante@gmail.com>
2023-04-28 18:15:12 -0300

    Functions Commit

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,31 @@
-using System;
+DisplayWelcome();
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep5 World!");
-    }
+string name = PromptName();
+int number = PromptNum();
+int sqr = SquareNum(number);
+DisplayResult(name, sqr);
+
+static void DisplayWelcome(){
+    System.Console.WriteLine("Welcome to the program!");
+}
+
+static string PromptName(){
+    System.Console.WriteLine("What is your name: ");
+    string name = Console.ReadLine();
+    return name;
+}
+
+static int PromptNum(){
+    System.Console.WriteLine("What is favorite number: ");
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+static int SquareNum(int number){
+    int sqr = number * number;
+    return sqr;
+}
+
+static void DisplayResult(string name, int sqr){
+    System.Console.WriteLine($"Brother {name}, the square of your number is {sqr}");
 }
