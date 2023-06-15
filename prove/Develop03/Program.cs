4df065e5e76803b754bbc22ebe136f2be506543e
David Basante <dsbasante@gmail.com>
2023-06-15 14:35:39 -0300

    late commit

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,35 @@
 using System;
+namespace Develop03;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Console.Clear();
+        Console.WriteLine("Welcome to Scripture Memorize \n");
+
+        var scripture = new Scripture();
+
+        // Adicione aqui os versículos da escritura
+        scripture.AddVerse("John 3:16", "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.");
+
+        Console.WriteLine("Press Enter to hide words. Type 'quit' to quit.");
+        Console.ReadLine();
+
+        var wordCountToHide = 3; // Quantidade de palavras a serem ocultadas em cada versículo
+
+        while (scripture.GetHiddenVerseCount() < scripture.GetVerseCount())
+        {
+            Console.Clear();
+            var verse = scripture.GetRandomHiddenVerse();
+            scripture.HideWordsInVerse(verse, wordCountToHide);
+            scripture.DisplayScripture();
+
+            Console.WriteLine("\nPress Enter to hide more words. Type 'quit' to quit.");
+            var userInput = Console.ReadLine();
+
+            if (userInput.ToLower() == "quit")
+                break;
+        }
     }
 }
---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Develop03
+{
+    public class Word
+    {
+    public string Reference { get; set; }
+    public string Text { get; set; }
+    public bool Hidden { get; set; }
+
+    public Word(string reference, string text)
+    {
+        Reference = reference;
+        Text = text;
+        Hidden = false;
+    }
+    }
+}
